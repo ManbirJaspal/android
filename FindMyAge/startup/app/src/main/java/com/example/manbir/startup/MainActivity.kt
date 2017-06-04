0835d6dd5fe55ac4e67e9acc1ab0a412034216cb
ManbirJaspal <manb.jaspal@gmail.com>
2017-06-04 21:41:01 +0530

    created findmyage app

--- FindMyAge/startup/app/src/main/java/com/example/manbir/startup/MainActivity.kt
+++ FindMyAge/startup/app/src/main/java/com/example/manbir/startup/MainActivity.kt
@@ -2,13 +2,33 @@ package com.example.manbir.startup
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+open class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
+       // getmyage.setOnClickListener( View.OnClickListener {     //setOnClickListener is used when the button needs to be clickable
+
+
+       //})
     }
+
+     fun BuClickEvent(view: View) {
+
+        //fires on click of button
+
+        val userDOB:String = DOB.text.toString()            //Reading input
+
+        val year:Int = Calendar.getInstance().get(Calendar.YEAR)   //taking current year from system calendar
+        val myAge = year-userDOB.toInt()
+        tvShowAge.text = "Your age is " + myAge
+    }
+
 }
+
